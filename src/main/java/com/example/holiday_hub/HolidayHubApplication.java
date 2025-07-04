9864109117f6cfe66827a80f113b3bc0b244dce8
Lee Min Woo <lmw7414@naver.com>
2025-07-04 20:18:35 +0900

    project init

---
+++ src/main/java/com/example/holiday_hub/HolidayHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.holiday_hub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HolidayHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolidayHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/holiday_hub/HolidayHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.holiday_hub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolidayHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
