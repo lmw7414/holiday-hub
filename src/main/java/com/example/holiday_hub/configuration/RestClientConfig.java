dea940d2e28eefb79774197b9240875cc269bb2c
Lee Min Woo <lmw7414@naver.com>
2025-07-09 20:53:46 +0900

    #8 - feat: 실패한 요청에 대해 재처리 로직 추가
    fetch가 정상적으로 이뤄지지 않았을 경우 다시 fetch하도록 @Retryable 어노테이션을 추가해줬고, API 서버와의 readTimeout을 걸어 3초가 지나면 실패로 간주하고 다음 작업을 진행하도록 하였다.

--- src/main/java/com/example/holiday_hub/configuration/RestClientConfig.java
+++ src/main/java/com/example/holiday_hub/configuration/RestClientConfig.java
@@ -3,10 +3,12 @@ package com.example.holiday_hub.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestClient;
 
 import java.nio.charset.StandardCharsets;
+import java.time.Duration;
 import java.util.List;
 
 @Configuration
@@ -14,7 +16,10 @@ public class RestClientConfig {
 
     @Bean
     public RestClient restClient() {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setReadTimeout(Duration.ofSeconds(3));
         return RestClient.builder()
+                .requestFactory(factory)
                 .messageConverters(converter -> converter.add(createJsonConverter()))
                 .build();
     }
