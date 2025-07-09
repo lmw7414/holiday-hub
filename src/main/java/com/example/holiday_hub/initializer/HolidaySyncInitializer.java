f9daaa1b01495ca70aa667e3bcdd771e87ebbbc9
Lee Min Woo <lmw7414@naver.com>
2025-07-09 20:52:04 +0900

    #8 - feat: 공휴일 fetch 비동기 작업으로 변경
    동기 작업으로 처리시 1개의 년도 그리고 117개국을 모두 처리하는 데 20초 이상이 소요되었다.
    
    해당 작업의 특성은 IO 작업이므로 비동기로 작업을 처리하도록 변경했고, 2020년~2025년까지 117개국에 대해 DB에 저장하는데 까지 시간은 5초로 단축시켰다.

--- src/main/java/com/example/holiday_hub/initializer/HolidaySyncInitializer.java
+++ src/main/java/com/example/holiday_hub/initializer/HolidaySyncInitializer.java
@@ -14,6 +14,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 @Slf4j
 @Component
@@ -31,22 +34,41 @@ public class HolidaySyncInitializer {
     @Transactional
     @EventListener(ApplicationReadyEvent.class)
     public void onApplicationReady() {
-        syncAll(fromYear, toYear);
+        List<String> countryCodes = syncCountry();
+        syncAll(countryCodes, fromYear, toYear);
     }
 
-    public void syncAll(int fromYear, int toYear) {
-        // TODO: 속도 개선 작업 REFACTORING 필요
+    public void syncAll(List<String> countryCodes, int fromYear, int toYear) {
+        ExecutorService excutor = Executors.newFixedThreadPool(20);
+        List<Future<List<HolidayInfoDto>>> futures = new ArrayList<>();
+
+        for (int year = fromYear; year <= toYear; year++) {
+            for (String countryCode : countryCodes) {
+                final int finalYear = year;
+                futures.add(excutor.submit(() -> client.fetchHolidays(finalYear, countryCode)));
+            }
+            log.info("{}년 fetch 요청", year);
+        }
+
+        List<HolidayInfoDto> holidayInfoDtos = new ArrayList<>();
+        for (Future<List<HolidayInfoDto>> future : futures) {
+            try {
+                holidayInfoDtos.addAll(future.get()); // future 작업 마칠 때까지 대기
+            } catch (Exception e) {
+                log.warn("공휴일 정보 조회 실패", e);
+            }
+
+        }
+        excutor.shutdown();
+        holidayInfoRepository.saveAll(holidayInfoDtos.stream().map(HolidayInfoDto::toEntity).toList());
+        log.info("{}개 공휴일 DB 업데이트 성공", holidayInfoDtos.size());
+    }
+
+    public List<String> syncCountry() {
         List<CountryInfoDto> countriesDtos = client.fetchCountries();
         countryInfoRepository.saveAll(countriesDtos.stream().map(CountryInfoDto::toEntity).toList());
-        log.info("국가 정보 업로드 완료: {}개",countriesDtos.size());
-        List<HolidayInfoDto> holidayInfoDtos = new ArrayList<>();
-        for (int year = fromYear; year <= toYear; year++) {
-            for (CountryInfoDto country : countriesDtos) {
-                holidayInfoDtos.addAll(client.fetchHolidays(year, country.countryCode()));
-            }
-            log.info("{}년 정보 업데이트 완료", year);
-        }
-        holidayInfoRepository.saveAll(holidayInfoDtos.stream().map(HolidayInfoDto::toEntity).toList());
+        log.info("국가 정보 업로드 완료: {}개", countriesDtos.size());
+        return countriesDtos.stream().map(CountryInfoDto::countryCode).toList();
     }
 
 
